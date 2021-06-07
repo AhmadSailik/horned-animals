219b9d1ced88f141419306d01c6ccdbe4b331e7a
AhmadSailik <asailik1993@gmail.com>
2021-06-07 14:22:20 +0300

    add josn file and defin on main and hornpage

--- src/component/HornedBeast.js
+++ src/component/HornedBeast.js
@@ -1,11 +1,26 @@
 import React from 'react';
 class HornedBeast extends React.Component{
+
+constructor(pops){
+    super(pops);
+    this.state={
+        numberOfClick:0
+    }
+}
+numberOfClickFuc=()=>{
+    this.setState({
+        numberOfClick:this.state.numberOfClick+1
+    })
+}
+
     render(){
         return(
             <div>
                 <h2>{this.props.hornTitle}</h2>
                 <p>{this.props.hornDescription}</p>
-                <img src={this.props.hornImage} />
+                <img src={this.props.hornImage} onClick={this.numberOfClickFuc} />
+                <p>numberOfClick:{this.state.numberOfClick}</p>
+
             </div>
         )
     }
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import HornedBeast from './HornedBeast'
+import HornedBeast from './HornedBeast';
+import rawData from './data.json';
 class Main extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            gallry:rawData
+        }
+    }
     render(){
         return(
             <div>
+                {rawData.map((item,idx)=>{
+                    return(
+                        <HornedBeast 
+                        hornTitle={item.title}
+                        hornDescription={item.description}
+                        hornImage={item.image_url}
+                        />
+ 
+                    )
+                })}
                 
-                <HornedBeast hornTitle={'UniWhal'}/>
-                <HornedBeast hornDescription={'A unicorn and a narwhal nuzzling their horns'}/>
-                <HornedBeast hornImage={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'}/>
-                <HornedBeast hornTitle={'Rhino Family'}/>
-                <HornedBeast hornDescription={'Mother (or father) rhino with two babies'}/>
-                <HornedBeast hornImage={'https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'}/>
-                <HornedBeast hornTitle={'Unicorn Head'}/>
-                <HornedBeast hornDescription={'Someone wearing a creepy unicorn head mask'}/>
-                <HornedBeast hornImage={'https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg'}/>
+
             </div>
         )
     }
